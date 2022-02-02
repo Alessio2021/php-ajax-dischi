896158372db981572b13d9961f2441e12a65677f
Alessio2021 <alessiobut1911@gmail.com>
2022-02-02 16:21:06 +0100

    milestone 2

--- partials/main.php
+++ partials/main.php
@@ -9,10 +9,18 @@
 </head>
 
 <body>
-    <main>
+    <main id="app">
         <div class="container-60">
             <div class="row row-cols-5 px-5 pt-5">
-                <?php include_once __DIR__ . '/cards.php' ?>
+                <!-- <?php include_once __DIR__ . '/cards.php' ?> -->
+                <div v-for="card in cards" class="col text-center p-3">
+                    <div class="card cards p-3">
+                        <img :src="card.poster" :alt="card.title">
+                        <h1 class="fw-bold text-light fs-4 pt-2">{{card.title}}</h1>
+                        <h5 class="text-secondary">{{card.author}}</h5>
+                        <h6 class="text-secondary">{{card.year}}</h6>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
